c7ffc57e5f701044af3771362c6ba719604f4e17
Js-Carvalho <Joassilva120@gmail.com>
2024-08-24 13:22:05 -0300

    aula 2

---
+++ api/src/main/java/br/com/fatecommerce/api/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package br.com.fatecommerce.api.repository;
+
+import br.com.fatecommerce.api.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ api/src/main/java/br/com/fatecommerce/api/service/CategoryService.java
@@ -0,0 +1,23 @@
+package br.com.fatecommerce.api.service;
+
+import br.com.fatecommerce.api.entity.Category;
+import br.com.fatecommerce.api.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.saveAndFlush(category);
+    };
+
+    public List<Category> getInfoCategorias() {
+        return categoryRepository.findAll();
+    }
+}
