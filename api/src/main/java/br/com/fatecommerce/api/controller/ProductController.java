165898b457b69efa278f55310556db1787d33e14
Js-Carvalho <Joassilva120@gmail.com>
2024-09-11 20:02:04 -0300

    + 3 rotas implementadas - findbyean/findbysku/findbyname

--- api/src/main/java/br/com/fatecommerce/api/controller/ProductController.java
+++ api/src/main/java/br/com/fatecommerce/api/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/product")
@@ -45,6 +46,24 @@ public class ProductController {
     @PutMapping(value = "/update")
     public ResponseEntity<Object> updateProduct(@RequestBody Product product) {
         Product result = productService.updateProduct(product);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping(value = "/findProductByEan/{eanProduct}")
+    public ResponseEntity<Object> getProductByEan(@PathVariable String eanProduct){
+        Optional<Product> result = productService.findByEanProduct(eanProduct);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping(value = "/findProductBySkuProduct/{skuProduct}")
+    public ResponseEntity<Object> getProductBySkuProduct(@PathVariable String skuProduct){
+        List<Product> result = productService.findBySkuProduct(skuProduct);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping(value = "/findProductNameProduct/{nameProduct}")
+    public ResponseEntity<Object> getProductNameProduct(@PathVariable String nameProduct){
+        List<Product> result = productService.findByNameProduct(nameProduct);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
--- api/src/main/java/br/com/fatecommerce/api/service/ProductService.java
+++ api/src/main/java/br/com/fatecommerce/api/service/ProductService.java
@@ -19,7 +19,9 @@ public class ProductService {
 
     public Product saveProduct(Product product) {
         return productRepository.saveAndFlush(product);
-    };
+    }
+
+    ;
 
     public List<Product> getInfoProducts() {
         return productRepository.findAll();
@@ -35,7 +37,7 @@ public class ProductService {
 
         productRepository.delete(product.get());
         HashMap<String, Object> result = new HashMap<String, Object>();
-        result.put("result", "Produto: " + product.get().getNameProduct() +  " excluído com sucesso!");
+        result.put("result", "Produto: " + product.get().getNameProduct() + " excluído com sucesso!");
         return result;
     }
 
@@ -46,8 +48,38 @@ public class ProductService {
     }
 
     public Product updateProduct(Product product) {
-        if(findProductById(product.getIdProduct()) != null) {
+        if (findProductById(product.getIdProduct()) != null) {
             return productRepository.saveAndFlush(product);
+        } else {
+            throw new ResponseStatusException((HttpStatus.NOT_FOUND), "Produto não encontrado");
+        }
+    }
+
+    public Optional<Product> findByEanProduct(String eanProduct) {
+        return Optional.ofNullable(productRepository.findByEanProduct(eanProduct)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Nenhum produto encontrado com esse EAN!")));
+    }
+
+    public List<Product> findBySkuProduct(String skuProduct) {
+        if (skuProduct != null) {
+            List<Product> list = productRepository.findBySkuProduct(skuProduct);
+            if (list.isEmpty()) {
+                throw new ResponseStatusException((HttpStatus.NOT_FOUND), "Lista de produtos está vazia!");
+            }
+            return list;
+        } else {
+            throw new ResponseStatusException((HttpStatus.NOT_FOUND), "SKU não encontrado");
+        }
+    }
+
+    public List<Product> findByNameProduct(String nameProduct) {
+        if (nameProduct != null) {
+            List<Product> list = productRepository.findByNameProductIgnoreCaseContaining(nameProduct);
+            if (list.isEmpty()) {
+                throw new ResponseStatusException((HttpStatus.NOT_FOUND), "Lista de produtos está vazia!");
+            }
+            return list;
         } else {
             throw new ResponseStatusException((HttpStatus.NOT_FOUND), "Produto não encontrado");
         }
